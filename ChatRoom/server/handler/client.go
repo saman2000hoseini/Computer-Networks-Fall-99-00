46288d6623a29811b44dc95f1dd2a0f0b8bf92aa
saman <Saman.Hoseini@snapp.cab>
2020-10-28 02:36:36 +0330

    fix: fix client handler client type

--- ChatRoom/server/handler/client.go
+++ ChatRoom/server/handler/client.go
@@ -3,10 +3,10 @@ package handler
 import "github.com/saman2000hoseini/Computer-Networks-Fall-99-00/ChatRoom/server/model"
 
 type ClientHandler struct {
-	Client model.Client
+	Client *model.Client
 }
 
-func NewClientHandler(client model.Client) ClientHandler {
+func NewClientHandler(client *model.Client) ClientHandler {
 	return ClientHandler{Client: client}
 }
 
