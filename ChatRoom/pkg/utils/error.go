7d3c44b2d0fc7edc7c49da0c55c43e0d507f3ced
saman <Saman.Hoseini@snapp.cab>
2020-10-30 03:21:26 +0330

    feat: add errors package

---
+++ ChatRoom/pkg/utils/error.go
@@ -0,0 +1,7 @@
+package utils
+
+const (
+	ErrWrongPassword = "the password you have entered is incorrect"
+	ErrInternal      = "error while processing request"
+	ErrUserNotFound  = "username not found"
+)
